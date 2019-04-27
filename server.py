41681823688d963c9473ec528e1ccf49205b31be
Silvia Scalisi <silvia.scalisi@gmail.com>
2019-04-27 11:10:58 +0200

    Routing

--- server.py
+++ server.py
@@ -1,12 +1,93 @@
 import cherrypy
+import sqlite3
+
+def CORS():
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "http://localhost"
+
+def db_connect():
+    conn = sqlite3.connect('polls.db')
+    c = conn.cursor()
+
+
+@cherrypy.tools.json_out()
+@cherrypy.popargs('user_id', 'poll_id')
+class Vote(object):
+    def __init__(self):
+        pass
+
+    @cherrypy.expose
+    def create_or_edit_vote(self, user_id, poll_id):
+        return {"message": "created vote"}
+
+    @cherrypy.expose
+    def poll_user_vote(self, poll_id, user_id):
+        return {"message": "list user votes"}
+
+@cherrypy.popargs('user_id')
+class Users(object):
+    def __init__(self):
+        pass
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def list_user_polls(self, user_id):
+        return {"message" : "listing user polls"}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def poll_user_vote(self):
+        return {"message": "list of users vote"}
 
 
 class Poll(object):
     @cherrypy.expose
-    def index(self):
-        return "Hello world!"
+    @cherrypy.tools.json_out()
+    def create_or_edit_poll(self):
+        return {"message_poll" :"calling post method"}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.popargs('poll_id')
+    def polls_details(self, poll_id):
+        return {"ciao": "calling get method"}
 
 
 if __name__ == '__main__':
-    cherrypy.config.update("server.conf")
-    cherrypy.quickstart(Poll(), "/polls")
+    db_connect()
+    cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
+    d = cherrypy.dispatch.RoutesDispatcher()
+
+    d.connect("default", route="/",
+                 controller=Poll(),
+                 action='create_or_edit_poll',
+                 conditions=dict(method=['POST']))
+
+    d.connect("polls_details", route="/{poll_id}",
+                 controller=Poll(),
+                 action='polls_details',
+                 conditions=dict(method=['GET']))
+
+
+    d.connect("user_polls", route="/users/{user_id}",
+                 controller=Users(),
+                 action='list_user_polls',
+                 conditions=dict(method=['GET']))
+
+    d.connect("user_vote", route="/{poll_id}/users/{user_id}/vote",
+                 controller=Vote(),
+                 action='create_or_edit_vote',
+                 conditions=dict(method=['POST']))
+
+    d.connect("user_votes", route="/{poll_id}/users/{user_id}",
+                 controller=Vote(),
+                 action='poll_user_vote',
+                 conditions=dict(method=['GET']))
+
+    conf = {'/': {
+            'request.dispatch': d,
+            'tools.CORS.on': True
+            }
+    }
+
+    cherrypy.tree.mount(Poll(), '/polls', config=conf)
+    cherrypy.engine.start()
